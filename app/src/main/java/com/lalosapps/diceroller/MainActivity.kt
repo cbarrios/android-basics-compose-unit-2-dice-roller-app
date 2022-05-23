dedad7e3daa6e1d8c5819335e396bb8d017ff2f0
cbarrios <lalobarrios16@yahoo.com>
2022-05-23 17:05:22 -0400

    master: (6) Add an image

--- app/src/main/java/com/lalosapps/diceroller/MainActivity.kt
+++ app/src/main/java/com/lalosapps/diceroller/MainActivity.kt
@@ -3,9 +3,8 @@ package com.lalosapps.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,8 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lalosapps.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,6 +52,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {}) {
             Text(text = stringResource(R.string.roll))
         }
