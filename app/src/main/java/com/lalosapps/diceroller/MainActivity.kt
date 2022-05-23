cf3aaa4ff1add40bb7d56bdb341c45d1e7a16bbc
cbarrios <lalobarrios16@yahoo.com>
2022-05-23 16:57:27 -0400

    master: (5) Add a button

--- app/src/main/java/com/lalosapps/diceroller/MainActivity.kt
+++ app/src/main/java/com/lalosapps/diceroller/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.lalosapps.diceroller.ui.theme.DiceRollerTheme
 
@@ -48,6 +51,8 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.roll))
+        }
     }
 }
